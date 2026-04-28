d51ac425e9c54d57b666bcc6659cb1ca27f5613d
Belenov Dmitriy <dimabelenov@gmail.com>
2026-04-28 21:36:10 +0300

    feat: implement Home and About pages with navigation links

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+	return (
+		<div>
+			<Link href="/" className="text-blue-500 hover:text-blue-700">Home</Link>
+			<h1>About</h1>
+		</div>
+	);
+}
